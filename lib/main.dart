fdc9b4dac4cfdd34a8296c6b7b662eace119e554
cyber-venom003 <tech.worm.1728@gmail.com>
2021-01-12 13:14:14 +0530

    Added Landing Pages

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Options(),
+        home: LandingPage(),
+        theme: ThemeData(
+          primarySwatch: Colors.purple
+        )
     );
   }
 }
--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -1,36 +1,65 @@
+import 'package:data_app/screens/options_page.dart';
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatelessWidget {
   final _controller = PageController(initialPage: 0);
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: PageView(
-        controller: _controller,
-        children: [
-          Scaffold(
-            body: Center(
-              child: Card(
-                child: Text('1'),
-              ),
+    return PageView(
+      controller: _controller,
+      children: [
+        Scaffold(
+          body: Center(
+            child: Image(
+              image: AssetImage('assets/images/Group_23.png'),
+            )
+          ),
+        ),
+        Scaffold(
+          body: Center(
+            child: Image(
+              image: AssetImage('assets/images/Group_27.png'),
             ),
           ),
-          Scaffold(
-            body: Center(
-              child: Card(
-                child: Text('1'),
-              ),
-            ),
+        ),
+        Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 82.5,),
+                Image(
+                  image: AssetImage('assets/images/Group_26.png'),
+                ),
+                SizedBox(height: 10,),
+                FlatButton(
+                  onPressed: () {
+                    Navigator.push(context , MaterialPageRoute(builder: (context) => Options()));
+                  },
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF7340DD),
+                      borderRadius: BorderRadius.circular(17),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                      child: Text(
+                        'Get Started',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
           ),
-          Scaffold(
-            body: Center(
-              child: Card(
-                child: Text('1'),
-              ),
-            ),
-          )
-        ],
-      )
+        )
+      ],
     );
   }
 }
